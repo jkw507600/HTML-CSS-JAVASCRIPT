1f8da0a67d79ca1a2552c7d76021e21348bab3e7
jkw507600 <jkw507600@gmail.com>
2023-11-01 17:28:28 +0900

    updated

---
+++ Javascript/basic/03_condition.js
@@ -0,0 +1,41 @@
+// { 
+//     const age = prompt ('당신의 나이는?')
+//     if( age >= 20 ){
+//         document.write('성인입니다.')
+//     } else{
+//         document.write('청소년 입니다.')
+//     } 
+// }
+
+// {
+//     const month = prompt ('지금은 몇월 입니까')
+//     if( month >= 3 && month <= 5 ){
+//         console.log ('봄')
+//     } else if ( month >= 6 && month <= 8 ){
+//         console.log ('여름')
+//     } else if ( month >= 9 && month <= 11 ){
+//         console.log ('가을')
+//     } else {
+//         console.log ('겨울')
+//     }
+// }
+
+// {
+//     const site = prompt ('네이버, 구글, 다음 중에서 즐겨 사용하느 포털 사이트는?')
+
+//     switch (site) {
+//         case '네이버':
+//             url = 'https:naver.com'
+//             break;
+//         case '구글':
+//             url = 'https:google.com'
+//             break;
+//         case '다음':
+//             url = 'https:daum.com'
+//             break;
+//         default :
+//             break;    
+//     }
+
+//     document.write('<a href=${url}>${site}</a>')
+// }
